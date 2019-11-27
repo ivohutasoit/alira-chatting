ac5ebc9f38849f64a237feb61ecf3faa5cafbef0
Ivo Hutasoit <if09051@gmail.com>
2019-11-27 08:30:39 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
-	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/ivohutasoit/alira-chatting/service"
 
-	"github.com/ivohutasoit/alira-chatting/model"
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		//port = "9000"
-		log.Fatal("$PORT must be set")
+		port = "9000"
+		//log.Fatal("$PORT must be set")
 	}
 
 	router := gin.New()
@@ -29,61 +27,12 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
-	router.GET("/ws", func(c *gin.Context) {
-		handleConnection(c.Writer, c.Request)
+	room := service.CreateChatRoom()
+	chat := service.CreateSavedChat()
+
+	router.GET("/channel/:id", func(c *gin.Context) {
+		service.StartChatRoom(room, chat)
 	})
 
-	go handleMessages()
-
 	router.Run(":" + port)
-}
-
-var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan model.Message)
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func handleConnection(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
-		//fmt.Println("Failed to set web socket upgrade: %+v", err)
-		//return
-	}
-
-	defer conn.Close()
-
-	// Register new client
-	clients[conn] = true
-
-	for {
-		var msg model.Message
-		err := conn.ReadJSON(&msg)
-		if err != nil {
-			log.Printf("Error Read: %v", err)
-			delete(clients, conn)
-			break
-		}
-		broadcast <- msg
-	}
-}
-
-func handleMessages() {
-	for {
-		msg := <-broadcast
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				log.Printf("Error Write: %v", err)
-				client.Close()
-				delete(clients, client)
-			}
-		}
-	}
 }
