2b541e9ed7be64c041b40fe83f58bcfd6eac5b14
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2019-12-03 17:57:53 +0800

    Add middleware and session

--- main.go
+++ main.go
@@ -1,44 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/ivohutasoit/alira-chatting/service"
-
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/ivohutasoit/alira-chatting/service"
+	"github.com/ivohutasoit/alira/middleware"
+	"github.com/ivohutasoit/alira/util"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		port = "9000"
-		//log.Fatal("$PORT must be set")
+		fmt.Println("$PORT must be set")
+		port = "9001"
 	}
 
 	router := gin.New()
 	router.Use(gin.Logger())
+	router.Use(sessions.Sessions("ALIRASESSIOn", util.Store))
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+		/*c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
 			"token": service.GenerateToken(),
+		})*/
+		http.ServeFile(c.Writer, c.Request, "templates/home.html")
+	})
+
+	api := router.Group("/api/alpha")
+	{
+		room := service.CreateChatRoom()
+		chat := service.CreateSavedChat()
+		api.Use(middleware.AuthenticationRequired())
+
+		api.GET("/channel/:id", func(c *gin.Context) {
+			service.StartChatRoom(room, chat)
 		})
-		//http.ServeFile(c.Writer, c.Request, "templates/home.tmpl.html")
-	})
-	router.GET("/api/alpha/qrcode/generate/:token", service.GenerateQRCode)
-	router.GET("/api/alpha/auth/socket/:token", service.ActivateSocket)
-	router.GET("/api/alpha/auth/token/:token", service.ValidateToken)
-
-	room := service.CreateChatRoom()
-	chat := service.CreateSavedChat()
-
-	router.GET("/channel/:id", func(c *gin.Context) {
-		service.StartChatRoom(room, chat)
-	})
+	}
 
 	router.Run(":" + port)
 }
